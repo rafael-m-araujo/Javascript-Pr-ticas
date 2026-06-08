98ca18a51dd8e99349c6c01da3f2c0fa332b3cc5
Rafael Mendes Araújo <rafaelmendes0006@gmail.com>
2026-06-08 05:53:40 -0300

    Um Pouco de Estudos

--- 5_ARRAYS_E_OBJETOS/js/script.js
+++ 5_ARRAYS_E_OBJETOS/js/script.js
@@ -2,3 +2,29 @@
 const lista = [1, 2, 3, 4, 5];
 
 console.log(lista);
+
+console.log(typeof lista);
+
+const itens = ["Rafael", true, 2, 4.12, []];
+
+console.log(itens);
+
+// 2 - Mais sobre Arraays
+const arr = ["a", "b", "c", "d", "d"]
+
+console.log(arr[0]);
+
+console.log(arr[2]);
+
+console.log(arr[83]);
+
+// 3 - Propriedades
+const numbers = [5, 3, 4];
+
+console.log(numbers.length);
+
+console.log(numbers["length"]);
+
+const myName = "Rafael";
+
+console.log(myName.length);
