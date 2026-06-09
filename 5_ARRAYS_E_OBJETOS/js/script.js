f56c252673f393c46f61e1b0b1f0d4e10657e886
Rafael Mendes Araújo <rafaelmendes0006@gmail.com>
2026-06-08 21:04:08 -0300

    Alguns estudos de Arrays e Objetos

--- 5_ARRAYS_E_OBJETOS/js/script.js
+++ 5_ARRAYS_E_OBJETOS/js/script.js
@@ -28,3 +28,196 @@ console.log(numbers["length"]);
 const myName = "Rafael";
 
 console.log(myName.length);
+
+// 4 - Métodos 
+const otherNumbers = [1, 2, 3]
+
+const allNumbers = numbers.concat(otherNumbers);
+
+console.log(allNumbers);
+
+const text = "algum texto";
+
+console.log(text.toUpperCase());
+
+console.log(typeof text.toUpperCase);
+
+console.log(text.indexOf("g"));
+
+// 5 - Objetos
+const person = {
+    name: "Rafael",
+    age: 20,
+    job: "Programador"
+}
+
+console.log(person);
+
+console.log(person.name);
+
+console.log(person.name.length);
+
+console.log(typeof person);
+
+// 6 - Criando e deletando propriedades
+const car = {
+    engine: 2.0,
+    brand: "VW",
+    model: "Tiaguan",
+    km: 2000,
+};
+
+console.log(car);
+
+car.doors = 4;
+
+console.log(car);
+
+delete car.km
+
+console.log(car);
+
+// 7 - Mais sobre objetos
+const obj = {
+    a:"teste",
+    b: true,
+};
+
+console.log(obj instanceof Object);
+
+const obj2 = {
+    c: [],
+};
+
+Object.assign(obj2, obj);
+
+console.log(obj2);
+
+console.log(obj);
+
+// 8 - Conhecendo melhor os objetos
+console.log(Object.keys(obj));
+console.log(Object.keys(obj2));
+console.log(Object.keys(car));
+
+console.log(Object.entries(car))
+
+// 9 - Mutação
+const a = {
+name: "Rafael",
+};
+
+const b = a
+
+console.log(a);
+console.log(b);
+
+console.log(a === b);
+
+a.age = 20;
+
+console.log(a);
+console.log(b);
+
+delete b.age
+console.log(a);
+console.log(b);
+
+// 10 - Loop em array
+const users = ["Rafael", "Miguel", "Gabriel", "Natanael"]
+
+for (let i = 0; i < users.length; i++) {
+    console.log(`Listando o usuário: ${users[i]}`);
+}
+
+// 11 - Push e Pop
+const array = ["a", "b", "c"];
+
+array.push("d");
+
+console.log(array);
+
+console.log(array.length);
+
+array.pop();
+
+console.log(array);
+
+const itemRemovido = array.pop();
+
+console.log(itemRemovido);
+
+console.log(array);
+
+array.push("z", "x", "y");
+
+console.log(array);
+
+// 12 - Shift e Unshift
+
+const letters = ["a", "b", "c"];
+
+const letter = letters.shift();
+
+console.log(letter);
+
+console.log(letters);
+
+letters.unshift("p", "q", "r")
+
+letters.unshift("z");
+
+console.log(letters);
+
+// 13 - IndexOf e lastIndexOf
+const myElements = ["morango", "mãça", "abacate", "pêra", "abacate"];
+
+console.log(myElements.indexOf("maçã"));
+console.log(myElements.indexOf("abacate"));
+
+console.log(myElements[2]);
+console.log(myElements[myElements.indexOf("abacate")]);
+
+console.log(myElements.lastIndexOf("abacate"));
+
+console.log(myElements.indexOf("Mamão"));
+
+console.log(myElements.lastIndexOf("Mamão"));
+
+// 14 - Slice
+const testeSlice = ["a", "b", "c", "d", "e", "f"];
+
+const subArray = testeSlice.slice(2, 4);
+
+console.log(subArray);
+
+console.log(testeSlice);
+
+const subArray2 = testeSlice.slice(2, 4 + 1);
+
+console.log(subArray2);
+
+const subArray3 = testeSlice.slice(10, 20);
+
+console.log(subArray3);
+
+const subArray4 = testeSlice.slice(2);
+
+console.log(subArray4);
+
+// 15 - foreach
+const nums = [1, 2, 3, 4, 5];
+
+nums.forEach((numero) => {
+    console.log(`O número é ${numero}`);
+});
+
+const posts = [
+{title: "Primeiro post", category: "PHP" },
+{title: "Segundo post", category: "JavaScript" },
+{title: "Terceiro post", category: "Python" },
+];
+
+posts.forEach((post) => {
+    console.log(`Exibindo post: ${post.title}, da categoria: ${post.category}`);
+});
