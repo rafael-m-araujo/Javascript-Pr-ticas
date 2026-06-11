159b5d0c688a506c59abbc9936241d58f7a1bdd6
Rafael Mendes Araújo <rafaelmendes0006@gmail.com>
2026-06-11 19:38:23 -0300

    Inicio de estudo DEBUG

---
+++ 7_DEBUG/js/script.js
@@ -0,0 +1,25 @@
+// 1 - Strict
+"use strict";
+
+// opa = "teste";
+
+const opa = "teste";
+
+// const undefined = 10;
+
+// delete [].length;
+
+// 2 - Console.log
+let a = 1;
+let b = 2;
+
+if (a == 1) {
+    a = b + 2;
+}
+
+console.log(a);
+
+for(let i = 0; i < b; i++) {
+    a = a + 2;
+    console.log(a);
+}
