57b9d4635be21dce2b2004332d10e3031c3aae6c
Rafael Mendes Araújo <rafaelmendes0006@gmail.com>
2026-06-13 09:46:34 -0300

    Quase finalização de async

---
+++ 8_ASYNC/js/script.js
@@ -0,0 +1,93 @@
+// // 1 - setTimeout
+// console.log("Ainda não executou")
+
+// setTimeout(function () {
+//     console.log("Requisisão assíncrona");
+// }, 2000);
+
+// console.log("Ainda não executou 2");
+
+// // //  2 - setInterval
+// // console.log("Ainda não começou")
+
+// // setInterval(function() {
+// //     console.log("Intervalo assíncrono")
+// // }, 3000)
+
+// // console.log("Ainda não começou 2")
+
+// // 3 - Promises
+// const promessa = Promise.resolve(5 + 5)
+
+// console.log("Algum código")
+
+// promessa.then(value => {
+//     console.log(`A soma é ${value}`);
+//     return value;
+// })
+// .then((value) => value -1)
+// .then((value) => console.log(`Agora é ${value}`));
+
+// console.log("Outro código")
+
+// // 4 - Falha na Promise
+// Promise.resolve(4 * "asd")
+// .then((n) => {
+
+// if (Number.isNaN(n)) {
+//     throw new Error("Valores inválidos");
+// }    
+
+// })
+// .catch((err) => console.log(`Um erro ocorreu: ${err}`));
+
+// 5 - Rejeição
+function checkNumber(n) {
+    return new Promise((resolve, reject) => {
+if (n > 10) {
+    resolve(`O número é maior que 10`);
+} else {
+    reject(new Error("Número muito baixo"));
+}
+ });
+}
+
+const a = checkNumber(20);
+
+const b = checkNumber(10);
+
+a.then((v) => console.log(`O resultado é ${v}`)).catch((err) => console.log(`Um erro ocorreu: ${err}`)
+);
+
+b.then((v) => console.log(`O resultado é ${v}`)).catch((err) => console.log(`Um erro ocorreu: ${err}`)
+);
+
+// 6 - Resolvendo promisses
+const p1 = new Promise((resolve, reject) => {
+    setTimeout(function() {
+        resolve(10)
+    }, 1000)
+})
+
+const p2 = Promise.resolve(10 + 10)
+
+const p3 = new Promise((resolve, reject) => {
+if(30 > 10) {
+    resolve(30)
+} else {
+    reject("Erro!");
+}
+});
+
+Promise.all([p1, p2, p3]).then((values) => console.log(values));
+
+// 7 - Async function
+async function somarComDelay(a, b) {
+    return a + b
+}
+
+somarComDelay(2, 4).then((value) => {
+console.log(`O valor da soma é: ${value}`);
+});
+
+console.log("teste async")
