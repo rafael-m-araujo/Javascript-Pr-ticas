0c347298202a6ab7909f0f3cb7d34b7e04a258e2
Rafael Mendes Araújo <rafaelmendes0006@gmail.com>
2026-06-15 17:05:49 -0300

    Finalização de Sessão de Eventos JS

---
+++ 10_EVENTOS/js/script.js
@@ -0,0 +1,137 @@
+// 1 - Adicionando Eventos
+const btn = document.querySelector("#my-button")
+
+btn.addEventListener("click", function() {
+
+    console.log("Clicou aqui!");
+
+});
+
+// 2 - Removendo evento
+const secondBtn = document.querySelector("#btn")
+
+function imprimirMensagem() {
+    console.log("Teste")
+}
+
+secondBtn.addEventListener("click", imprimirMensagem);
+
+const thirdBtn = document.querySelector("#other-btn");
+
+thirdBtn.addEventListener("click", () => {
+    console.log("Evento removido");
+    secondBtn.removeEventListener("click", imprimirMensagem);
+});
+
+// 3 - Argumento do Evento
+const myTitle = document.querySelector("#my-title")
+
+myTitle.addEventListener("click", (event) => {
+
+ console.log(event);
+ console.log(event.offsetX)
+ console.log(event.pointerType)
+ console.log(event.target);
+});
+
+// 4 - Propagação
+const containerBtn = document.querySelector("#btn-container")
+const btnInsideContainer = document.querySelector("#div-btn")
+
+containerBtn.addEventListener("click", () => {
+    console.log("Evento 1")
+});
+
+btnInsideContainer.addEventListener("click", (e) => {
+    e.stopPropagation();
+    console.log("Evento 2")
+});
+
+// 5 - Removendo Evento Padrão
+const a = document.querySelector("a")
+
+a.addEventListener("click", (e) => {
+
+ e.preventDefault()
+
+ console.log("Não alterou a página");
+});
+
+// 6 - Eventos de tecla
+document.addEventListener("keyup", (e) => {
+   console.log(`Soltou a tecla ${e.key}`);
+});
+
+document.addEventListener("keydown", (e) => {
+   console.log(`Apertou a tecla ${e.key}`);
+});
+
+// 7 - Eventos de mouse
+const mouseEvents = document.querySelector("#mouse")
+
+mouseEvents.addEventListener("mousedown", () => {
+    console.log("Pressionou o botão");
+})
+
+mouseEvents.addEventListener("mouseup", () => {
+    console.log("Soltou o botão");
+})
+
+mouseEvents.addEventListener("dblclick", () => {
+    console.log("Clique duplo");
+})
+
+// 8 - Movimento do mouse
+// document.addEventListener("mousemove", (e) => {
+//     console.log(`No eixo X: ${e.x}`)
+//     console.log(`No eixo Y: ${e.x}`)
+// })
+
+// 9 - Evento de scroll
+window.addEventListener("scroll", (e) => {
+    if(window.pageYOffset > 200) {
+      console.log("Passamos de 200px")
+    }
+});
+
+// 10 - Evento de focus/blur
+const input = document.querySelector("#my-input")
+
+input.addEventListener("focus", (e) => {
+  console.log("Entrou no input");
+});
+
+input.addEventListener("blur", (e) => {
+  console.log("Saiu no input");
+});
+
+// 11 - Evento de carregamento
+window.addEventListener("load", () => {
+    console.log("A página carregou!");
+})
+
+window.addEventListener("beforeunload", (e) => {
+    e.preventDefault();
+    e.returnValue = ""
+})
+
+// 12 - Debounce
+const debounce = (f, delay) => {
+  
+   let timeout 
+
+   return(...arguments) => {
+if(timeout) {
+    clearTimeout(timeout)
+}
+
+timeout = setTimeout(() => {
+  f.apply(arguments)
+}, delay);
+   };
+};
+
+window.addEventListener("mousemove", debounce(() => {
+    console.log("Excecutando a cada 400ms")
+}, 400)
+);
---
+++ 9_DOM/js/script.js
@@ -0,0 +1,103 @@
+// // 1 - Movendo-se pelo DOM
+// console.log(document.body);
+
+// console.log(document.body.childNodes[1]);
+
+// console.log(document.body.childNodes[1].childNodes);
+
+// console.log(document.body.childNodes[1].childNodes[1].textContent);
+
+// 2 - Selecionando por Tag
+const listItens = document.getElementsByTagName("li");
+
+// console.log(listItens);
+
+// // 3 - Selecionando elemento por id
+// const title = document.getElementById("title");
+
+// console.log(title);
+
+// console.log(title);
+
+// 4 - Selecionando elementos por classe
+const products = document.getElementsByClassName("product");
+
+console.log(products);
+
+// // 5 - Selecionando os elementos por CSS
+// const productsQuery = document.querySelectorAll(".product")
+
+// console.log(productsQuery);
+
+const mainContainer = document.querySelector("#main-container");
+
+// // 6 - insertBefore
+// const p = document.createElement("p");
+
+// console.log(p);
+
+// const header = title.parentElement;
+
+// header.insertBefore(p, title);
+
+// // 7 - appendChild
+// const navLinks = document.querySelector("nav ul")
+
+// const li = document.createElement("li")
+
+// navLinks.appendChild(li);
+
+// // 8 - replaceChild
+// const h2 = document.createElement("h2")
+
+// h2.textContent = "Meu novo título"
+
+// header.replaceChild(h2, title);
+
+// // 9 - createTextNode
+// const myText = document.createTextNode("Agora vamos colocar mais um título")
+
+// console.log(myText);
+
+// const h3 = document.createElement("h3")
+
+// h3.appendChild(myText)
+
+// console.log(h3);
+
+// mainContainer.appendChild(h3);
+
+// // 10 - Trabalhando com Atributos
+// const firstlink = navLinks.querySelector("a");
+
+// console.log(firstlink);
+
+// firstlink.setAttribute("href", "https://www.google.com");
+
+// console.log(firstlink.getAttribute("href"));
+
+// firstlink.setAttribute("target", "_blank");
+
+// // 11 - Altura e largura
+// const footer = document.querySelector("footer");
+
+// console.log(footer.offsetWidth);
+// console.log(footer.offsetHeight);
+
+// console.log(footer.clientWidth);
+// console.log(footer.clientHeight);
+
+// 12 - Posição do elemento
+const product1 = products[0];
+
+console.log(product1.getBoundingClientRect());
+
+// 13 - CSS com JS
+mainContainer.style.color = "red";
+mainContainer.style.backgroundColor = "#333";
+mainContainer.style.paddingBottom = "15px";
+
+// 14 - Alterando estilos de varios elementos
+for (const li of listItens) {
+    li.style.backgroundColor = "red";
+}
